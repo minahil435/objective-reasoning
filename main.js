971f3d2ca8db76a8a433113dd43f04ef2787d37e
Minahil shafique <minahal.shafique@gmail.com>
2021-03-10 14:44:44 -0500

    objects

--- main.js
+++ main.js
@@ -2,6 +2,55 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+function isAdmin(userObj) {
+    if (userObj.userRole === 'ADMIN') { return true }
+    return false
+}
+
+function getEmail(userObj) {
+    return (userObj.firstName).toLowerCase() + '.' + (userObj.lastName).toLowerCase() + "@codeimmersives.com"
+
+}
+
+function getPlaylistLength(playObj){
+    return playObj.songs.length
+    
+}
+
+function getHardestHomework(arrObj){
+    if (arrObj.length === 0) {return ""}
+      
+    let hardestHW = arrObj[0].name
+    let minScore = arrObj[0].averageScore
+    
+    
+    for (const eachObj of arrObj){
+        if (eachObj.averageScore < minScore){  
+            minScore =  eachObj.averageScore 
+            hardestHW =  eachObj.name   
+        }
+    }
+    return hardestHW
+}
+
+
+function createPhonebook(names , numbers){
+   
+
+    const obj = {
+
+    };
+
+    for (let i = 0 ; i < names.length ; i++  ){
+
+        obj[names[i]] = numbers[i]
+
+    }
+        
+        return obj
+
+}
+
 
 
 // ┌─────────────────────────────────────┐
